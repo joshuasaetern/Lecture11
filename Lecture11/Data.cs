99a5295978ddff45112b1472abeedbe08dcf0c8a
joshuasaetern <JoshuaSaetern17@gmail.com>
2024-05-26 23:55:42 -0700

    Add project files.

---
+++ Lecture11/Data.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture11
+{
+    internal class Data
+    {
+        //Fields
+        public static ObservableCollection<String> names = new ObservableCollection<string>();
+
+        //Constructors
+        static Data()
+        {
+            names.Add("Leo");
+            names.Add("Joshua");
+            names.Add("Jeremy");
+            names.Add("Tommy");
+            names.Add("Carl");
+        }
+        
+        //Properties
+
+        
+        //Methods
+    }
+}
